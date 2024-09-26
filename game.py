eb1236d55badfb409cb3dc54a108a296dae3c1f1
TheNovaStar <tera.alexandria.ward@gmail.com>
2024-09-25 20:53:45 -0400

    The game now correctly reads the json file. The only thing left to do is to finish the game engine.
    Also, I would like to implement more flavor text into the game. Specifically, the fight should play out differently depending on what items the player has. This is both for enhancing enjoyment, but also to let the player know what they were missing if they died.
    
    Also, there should be a short delay between steps so that the player can read. Maybe even waiting for the player to press enter so that the text advances.

--- game.py
+++ game.py
@@ -2,9 +2,10 @@ import json
 
 class Player:
     def __init__(self, items=None):
-        self.alive = True
         self.items = items
+        self.alive = True
         self.room = None
+        self.win = False
 
     def move(self, direction):
         """Causes the player to move between rooms. Assume 'direction' parameter is a player-inputted string.
@@ -15,9 +16,9 @@ class Player:
 
 
 class Room:
-    def __init__(self, room_name, items=None, enemy=None, room_text=None):
+    def __init__(self, room_name='notNamed', item=None, enemy=None, room_text=None):
         self.name = room_name
-        self.items = items
+        self.item = item
         self.north = None
         self.south = None
         self.east = None
@@ -30,11 +31,8 @@ class Room:
 
     def status(self):
         out_str = f'Room: {self.name}'
-        if self.items is not None:
-            out_str += ', Items:'
-            for item in self.items:
-                out_str += f', {item},'
-            out_str = out_str.strip(',')
+        if self.item is not None:
+            out_str += f', Item: {self.item}'
 
         if self.enemy is not None:
             out_str += f', Enemy: {self.enemy}'
@@ -51,10 +49,10 @@ class Room:
         if self.west is not None:
             out_str += f', West: {self.west}'
 
-        return out_str
+        print(out_str)
 
 class Item:
-    def __init__(self, name, item_text=None):
+    def __init__(self, name='notNamed', item_text=None):
         self.name = name
         self.text = item_text
 
@@ -62,12 +60,13 @@ class Item:
         return self.name
 
     def status(self):
-        return f'Item: {self.name}'
+       print(f'Item: {self.name}')
 
 class Enemy:
-    def __init__(self, name, defeat_items = None):
+    def __init__(self, name='notNamed', defeat_items = None, text = None):
         self.name = name
         self.defeat_items = defeat_items
+        self.text = text
         self.alive = True
 
     def __str__(self):
@@ -80,27 +79,114 @@ class Enemy:
             out_str += ' (alive)'
         else: out_str += ' (dead)'
 
-        return out_str
+        print(out_str)
 
     def fight(self):
         """Returns True if player wins, False otherwise."""
-        if self.defeat_items is not None:
-            for item in self.defeat_items:
-                if item in player.items: continue
-                else: return False
+        #TODO: implement fight flavor text for enemies
+        if self.alive:
+            if self.defeat_items is not None:
+                for item in self.defeat_items:
+                    if item in player.items: continue
+                    else: return False
+                else: return True
             else: return True
         else: return True
 
 if __name__ == '__main__':
-#FIXME: this part should be read from the json file, not hardcoded
 
-    #FIXME: read level data from .json file
+    #Read level data from file
+    #TODO: add a menu that reads level data from user input
+
+    with open('level.json', 'r') as level:
+        level_data = json.load(level)
+
+    #Initializes game
+
+    # Initialize all objects first, then assign relationships
+    for class_type in level_data.keys():
+        for entry in level_data[f'{class_type}'].keys():
+            exec(f'{entry.lower()} = {class_type.title().rstrip('s')}()')
 
 
+    for player in level_data['players'].keys():
+        eval(player).items = []
+        for item in level_data['players'][player]['starting_items']:
+            eval(player).items.append(eval(item))
 
-    #FIXME: initialize game
+        eval(player).room = eval(level_data['players'][player]['starting_room'])
+
+    for room in level_data['rooms'].keys():
+        eval(room).name = level_data['rooms'][room]['name']
+        eval(room).item = eval(level_data['rooms'][room]['item'])
+        eval(room).north = eval(level_data['rooms'][room]['north'])
+        eval(room).south = eval(level_data['rooms'][room]['south'])
+        eval(room).east = eval(level_data['rooms'][room]['east'])
+        eval(room).west = eval(level_data['rooms'][room]['west'])
+        eval(room).enemy = eval(level_data['rooms'][room]['enemy'])
+        eval(room).text = level_data['rooms'][room]['text']
+
+    for item in level_data['items'].keys():
+        eval(item).name = level_data['items'][item]['name']
+        eval(item).text = level_data['items'][item]['text']
+
+    # I used "enemys" instead of "enemies" so that I can use the .rstrip method to depluralize it.
+    for enemy in level_data['enemys'].keys():
+        eval(enemy).name = level_data['enemys'][enemy]['name']
+        eval(enemy).text = level_data['enemys'][enemy]['text']
+
+        eval(enemy).defeat_items = []
+        for item in level_data['enemys'][enemy]['defeat_items']:
+            eval(enemy).defeat_items.append(eval(item))
+
+    player = eval(f'{list(level_data['players'].keys())[0]}')
 
     #FIXME: create gameplay loop
+
+    # Display any one-time starting text for the game
+
     while True:
-        player.move(input('Enter a direction: '))
-        break
+    # for each room, display the room's description text.
+        print(player.room.text)
+        if player.room.item is not None:
+            print(player.room.item.text)
+
+    # if enemies are present, describe them. Then, fight them and describe the results. If the player loses the fight, the game is over.
+        if player.room.enemy is not None:
+            if player.room.enemy.alive:
+                print(player.room.enemy.text)
+                if not player.room.enemy.fight():
+                    player.alive = False
+                    break
+                else:
+                    player.room.enemy.alive = False
+            else:
+                #TODO: implement death text for enemies
+                ...
+
+
+    # FIXME:if the player defeated all enemies, they win!
+
+    # FIXME:if the game is not yet over, continue with the game:
+
+    # FIXME:if the room has an item, describe it and ask the player if they want to pick it up.
+
+    # prompt the player to choose where to go next.
+        player_move = input('Please enter a direction: North, South, West, East or Exit: ').lower().strip()
+        if player_move == 'exit':
+            break
+        elif eval(f'player.room.{player_move}') is not None:
+            player.room = eval(f'player.room.{player_move}')
+            print(f'Moved to {player.room}')
+
+        else: print('Please enter a valid direction!')
+
+    # When gameplay loop terminates, summarize the game and display text for winning, losing, and quitting.
+    if player.win:
+        print('You win!')
+    elif not player.alive:
+        print('You died!')
+
+    print('Thanks for playing!')
+
+    #TODO: Before main menu: add a "play again?" option. Once main menu implemented: return to the main menu after the summary.
